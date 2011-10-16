899f9ee85d23e45b08fab7eb3a5906bef5254060
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2011-10-16 16:45:36 +0200

    Initial commit.

---
+++ src/org/ktln2/android/LayoutExperimentsActivity.java
@@ -0,0 +1,15 @@
+package org.ktln2.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LayoutExperimentsActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
